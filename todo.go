ef433393dc39ff2d820811104cc2f9f73bd89dc8
GeorgiaMayday <georgiamaycawley@gmail.com>
2024-08-29 14:04:53 +0100

    Adding "add todo" functionality

--- todo.go
+++ todo.go
@@ -5,13 +5,19 @@ import (
 	"io"
 )
 
-type TodoList []string
+type TodoList struct {
+	list []string
+}
 
-func (tl TodoList) outputTodos(writer io.Writer) {
-	for i, todo := range tl {
+func (tl *TodoList) outputTodos(writer io.Writer) {
+	for i, todo := range tl.list {
 		fmt.Fprintln(writer, todo)
 		if i >= 9 {
 			break
 		}
 	}
+}
+
+func (tl *TodoList) addTodo(newTodo string) {
+	tl.list = append(tl.list, newTodo)
 }
--- todo_test.go
+++ todo_test.go
@@ -2,51 +2,72 @@ package todo
 
 import (
 	"bytes"
-	"cmp"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestTodoCLI(t *testing.T) {
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
+	if !cmp.Equal(got, want) {
+		t.Errorf("got todo list print %s but wanted %s", got, want)
+	}
+}
+
+func TestTodoOutput(t *testing.T) {
 	t.Run("10 todo's are printed", func(t *testing.T) {
-		todo_list := TodoList{"Iron", "Eat",
-			"Hunker", "Mine", "Shear", "Cut", "Griddle", "Cook", "Host", "Grate"}
+		todo_list := TodoList{[]string{"Iron", "Eat",
+			"Hunker", "Mine", "Shear", "Cut", "Griddle", "Cook", "Host", "Grate"}}
 		output := &bytes.Buffer{}
 
 		todo_list.outputTodos(output)
 
 		want := "Iron\nEat\nHunker\nMine\nShear\nCut\nGriddle\nCook\nHost\nGrate\n"
 
-		if cmp.Compare(output.String(), want) != 0 {
-			t.Errorf("got todo list print %s but wanted %s", output.String(), want)
-		}
+		assertStrings(t, output.String(), want)
+
 	})
 
 	t.Run("if less then 10 todo's ju", func(t *testing.T) {
-		todo_list := TodoList{"Iron", "Eat",
-			"Hunker", "Mine", "Shear", "Cut", "Griddle", "Cook", "Host", "Grate", "Scale", "Brush"}
+		todo_list := TodoList{[]string{"Iron", "Eat",
+			"Hunker", "Mine", "Shear", "Cut", "Griddle", "Cook", "Host", "Grate", "Scale", "Brush"}}
 		output := &bytes.Buffer{}
 
 		todo_list.outputTodos(output)
 
 		want := "Iron\nEat\nHunker\nMine\nShear\nCut\nGriddle\nCook\nHost\nGrate\n"
 
-		if cmp.Compare(output.String(), want) != 0 {
-			t.Errorf("got todo list print %s but wanted %s", output.String(), want)
-		}
+		assertStrings(t, output.String(), want)
 	})
 
 	t.Run("if less then 10 todo's ju", func(t *testing.T) {
-		todo_list := TodoList{"Iron", "Eat",
-			"Hunker", "Mine", "Shear", "Cut"}
+		todo_list := TodoList{[]string{"Iron", "Eat",
+			"Hunker", "Mine", "Shear", "Cut"}}
+
 		output := &bytes.Buffer{}
 
 		todo_list.outputTodos(output)
 
 		want := "Iron\nEat\nHunker\nMine\nShear\nCut\n"
-
-		if cmp.Compare(output.String(), want) != 0 {
-			t.Errorf("got todo list print %s but wanted %s", output.String(), want)
-		}
+		assertStrings(t, output.String(), want)
 	})
 
+}
+
+func TestAddTodo(t *testing.T) {
+	t.Run("if less then 10 todo's ju", func(t *testing.T) {
+		todo_list := TodoList{[]string{"Iron", "Eat",
+			"Hunker", "Mine", "Shear", "Cut"}}
+
+		todo_list.addTodo("Cook")
+
+		got := todo_list.list
+		want := []string{"Iron", "Eat",
+			"Hunker", "Mine", "Shear", "Cut", "Cook"}
+
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("got todo list print %s but wanted %s", got, want)
+		}
+
+	})
 }
